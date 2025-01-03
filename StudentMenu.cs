dc315ff3ef6852dfc0e98d3359e38de48980a89e
Zak Troy <zak.troy@chasacademy.se>
2025-01-03 17:58:27 +0100

    added Student menu with switch case

--- StudentMenu.cs
+++ StudentMenu.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SchoolDBProject.Models;
 
 namespace SchoolDBProject
 {
-    internal class StudentMenu
+    public class StudentMenu
     {
-    }
-}
+        public static void Show()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("=== Student Menu ===");
+                Console.WriteLine("1. List All Students");
+                Console.WriteLine("2. Add New Student");
+                Console.WriteLine("3. Students in a specific Class");
+                Console.WriteLine("4. Back to Main Menu");
+                Console.Write("Select an option: ");
+
+                var choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        ListStudents();
+                        break;
+                    case "2":
+                        AddStudent();
+                        break;
+                    case "3":
+                        ListStudentsInClass();
+                        break;
+                    case "4":
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option. Press Enter to try again.");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }
