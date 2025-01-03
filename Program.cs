b6169ab00530930b1d3dfbb1dc3398026d508401
Zak Troy <zak.troy@chasacademy.se>
2025-01-03 17:38:17 +0100

    made a quick main menu for the app and added the 3 classes that will contain those menus

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SchoolDBProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("=== School Database Management ===");
+                Console.WriteLine("1. Student Menu");
+                Console.WriteLine("2. Grade Menu");
+                Console.WriteLine("3. Personnel Menu");
+                Console.WriteLine("4. Exit");
+                Console.Write("Select an option: ");
+
+                var choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        StudentMenu.Show();
+                        break;
+                    case "2":
+                        GradeMenu.Show();
+                        break;
+                    case "3":
+                        PersonnelMenu.Show();
+                        break;
+                    case "4":
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option. Press Enter to try again.");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }
+    }
+}
