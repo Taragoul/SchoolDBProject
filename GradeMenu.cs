bb3aa32496bc7da953687cc152cfc78d44fca658
Zak Troy <zak.troy@chasacademy.se>
2025-01-03 17:59:42 +0100

    added grade menu with switch

--- GradeMenu.cs
+++ GradeMenu.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SchoolDBProject.Models;
 
 namespace SchoolDBProject
 {
-    internal class GradeMenu
+    public class GradeMenu
     {
-    }
-}
+        public static void Show()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("=== Grade Menu ===");
+                Console.WriteLine("1. List All Grades");
+                Console.WriteLine("2. Highest, Lowest, and Average Grade in a Class");
+                Console.WriteLine("3. Add New Grade");
+                Console.WriteLine("4. Back to Main Menu");
+                Console.Write("Select an option: ");
+
+                var choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        ListGrades();
+                        break;
+                    case "2":
+                        GradeStatistics();
+                        break;
+                    case "3":
+                        AddGrade();
+                        break;
+                    case "4":
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option. Press Enter to try again.");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }
