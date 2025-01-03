c3e0f7ddb7bb98994aa84967c5e40ad98088030d
Zak Troy <zak.troy@chasacademy.se>
2025-01-03 17:56:40 +0100

    added Personnel submenu with switch case

--- PersonnelMenu.cs
+++ PersonnelMenu.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SchoolDBProject.Models;
 
 namespace SchoolDBProject
 {
-    internal class PersonnelMenu
+    public class PersonnelMenu
     {
-    }
-}
+        public static void Show()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("=== Personnel Menu ===");
+                Console.WriteLine("1. List All Personnel");
+                Console.WriteLine("2. Add New Personnel");
+                Console.WriteLine("3. Teachers Per Department");
+                Console.WriteLine("4. Back to Main Menu");
+                Console.Write("Select an option: ");
+
+                var choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        ListPersonnel();
+                        break;
+                    case "2":
+                        AddPersonnel();
+                        break;
+                    case "3":
+                        CountTeachersByDepartment();
+                        break;
+                    case "4":
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option. Press Enter to try again.");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }
